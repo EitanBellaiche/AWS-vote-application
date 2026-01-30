1ea9c896941681c5aeb4b2a3ac79bac150cb6ea3
Eitan Bellaiche <eitanbellaiche@gmail.com>
2026-01-30 18:07:45 +0200

    update font family and color scheme for improved UI aesthetics

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
+import "./ui.css";
 
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { AuthProvider, useAuth } from "react-oidc-context";
